4c1db63d3ce99751a108680648dbdbf31a05899b
Justin Weidner <shadeban@gmail.com>
2012-10-21 13:46:41 -0500

    test

--- app/models/webpage.rb
+++ app/models/webpage.rb
@@ -2,24 +2,29 @@ require 'nokogiri'
 
 class Webpage < ActiveRecord::Base
 
-  def wordcount
- 	 
-	 node = Nokogiri.parse(response)
-	 @text = node.xpath("//text()") - node.xpath("//script//text()") - node.xpath("//style//text()")
-	 
-	words = text.inject([]) do |all,textnode|
-		textnode.text().scan(/(\$?\w+([\.@\-']\w+)*)/).each {|match| all << match}
-		all
-	end
+  WORD_REGEX =  /(\$?\w+([\.@\-']\w+)*)/
 
-
-	injdict = words.inject(Hash.new(0)) {|dict, match| dict[match.first.upcase] += 1; dict}
-	test = injdict.sort{|a,b| b[1] <=> a[1]}	
-	test[0...10]
-	
+  def word_count(n=10)
+	@word_count ||= sorted_word_count	
+	@word_count[0...10]
 	  
   end
-  def text
-	  @text ||= ""
+
+  def text_nodes
+	  
+	 node = Nokogiri.parse(response)
+	 node.xpath("//text()") - node.xpath("//script//text()") - node.xpath("//style//text()")
+
+  end	  
+
+  def sorted_word_count
+
+	  counts = text_nodes.inject(Hash.new(0)) do |word_counts, node|
+		node.text().scan(WORD_REGEX).each {|match| word_counts[match.first.upcase] += 1}
+		word_counts
+	  end
+	  counts.sort{|a,b| b[1] <=> a[1]}
+
   end
+
 end
